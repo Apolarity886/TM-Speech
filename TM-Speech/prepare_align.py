55e6c5b85f45a6e55c2ed75181e60c1683380170
Apolarity886 <dialog7508@gmail.com>
2024-09-09 14:36:55 +0800

    Update prepare_align.py

--- TM-Speech/prepare_align.py
+++ TM-Speech/prepare_align.py
@@ -10,13 +10,11 @@ def main(config):
         ljspeech.prepare_align(config)
     if "AISHELL3" in config["dataset"]:
         aishell3.prepare_align(config)
-    if "LibriTTS" in config["dataset"]:
-        libritts.prepare_align(config)
 
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
-    parser.add_argument("--configs", type=str, default='/home/wl/anaconda3/envs/audio/wl_windows/TM-Speech/configs/AISHELL3/preprocess.yaml', help="path to preprocess.yaml")
+    parser.add_argument("--configs", type=str, help="path to preprocess.yaml")
     args = parser.parse_args()
 
     config = yaml.load(open(args.configs, "r"), Loader=yaml.FullLoader)
