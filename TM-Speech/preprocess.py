f2ba0af7d531f373e40f60716ec63611e7596361
Apolarity886 <dialog7508@gmail.com>
2024-09-09 14:42:40 +0800

    Update preprocess.py

--- TM-Speech/preprocess.py
+++ TM-Speech/preprocess.py
@@ -6,7 +6,7 @@ from preprocessor.preprocessor import Preprocessor
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
-    parser.add_argument("--configs", type=str, default='/home/wl/anaconda3/envs/audio/wl_windows/TM-Speech/configs/AISHELL3/preprocess.yaml', help="path to preprocess.yaml")
+    parser.add_argument("--configs", type=str, default='./TM-Speech/configs/LJSpeech/preprocess.yaml', help="path to preprocess.yaml")
     args = parser.parse_args()
 
     config = yaml.load(open(args.configs, "r"), Loader=yaml.FullLoader)
